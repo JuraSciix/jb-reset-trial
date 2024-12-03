bbcfaa144909d0242f699c71a3c303b364f6ef96
JuraSciix <53406677+JuraSciix@users.noreply.github.com>
2024-12-03 07:03:30 +0300

    Update jb-reset-trial.ps1
    Fixes errors

--- jb-reset-trial.ps1
+++ jb-reset-trial.ps1
@@ -10,56 +10,53 @@
 
 $ideList = @("intellij", "phpstorm", "webstorm", "pycharm", "rider", "clion", "goland");
 
-$jetbrains = $env:APPDATA + "/JetBrains";
+$jetbrains = $env:APPDATA + "\JetBrains";
 
-Write-Host "Вычисление файлов: $jetbrains \n";
+Write-Host "Вычисление файлов: $jetbrains";
 
 $delete = @();
 
 $delete += "PermanentUserId";
 
-Get-ChildItem -Path $jetbrains 
-| ForEach-Object {
+Get-ChildItem -Path $jetbrains | ForEach-Object {
     $ide = $false
     foreach($prefix in $ideList){ 
-        if($_ -notlike "$prefix*") {
-            continue;
+        if($_ -like "$prefix*") {
+            $ide = $true;
+            break;
         }
     }
-    if(-not $ide){
-        exit 1;
+    if($ide){
+        $delete += "$_\options\other.xml";
     }
-
-    $delete += "$file/options/others.xml"
 }
 
 $map = @{}
 foreach($file in $delete){
     $realpath = "$jetbrains/$file"
-    if(-not (Test-Path $realpath -PathType Leaf)){
-        continue;
+    if(Test-Path $realpath -PathType Leaf){
+        $map[[char](65 + $map.Count)] = $file
     }
-    $map[[char](65 + $map.Count)] = $file
 }
 
-if($map.Count -eq 0){
-    exit 1
-}
-
-Write-Host "Файлы к удалению: "
-foreach($key in $map.Keys){
-    Write-Host "* ${key}: $($map.Keys[$key])" -ForegroundColor Red
-}
-Write-Host "" -NoNewline -ForegroundColor White
-
-$input_ = Read-Host -Prompt "Отправьте символы, соответствующие файлам, которые не следует удалять (или пропустите):`n";
-foreach ($sym in $input_.ToCharArray()) {
-    $map.Remove($sym)
-}
-Write-Host "Удаление файлов...";
-foreach($file in $map.Values){
-    Write-Host "- $file" -ForegroundColor Red
-    Remove-Item -Path $file
+if($map.Count -ne 0){
+    Write-Host "Файлы к удалению: "
+    foreach($key in $map.Keys){
+        Write-Host "* ${key}: $($map[$key])" -ForegroundColor Red
+    }
+    Write-Host "" -NoNewline -ForegroundColor White
+    
+    $input_ = Read-Host -Prompt "Отправьте символы, соответствующие файлам, которые не следует удалять (или пропустите):`n";
+    foreach ($sym in $input_.ToCharArray()) {
+        $map.Remove($sym)
+    }
+    
+    Write-Host "Удаление файлов...";
+    foreach($file in $map.Values){
+        $realpath = "$jetbrains\$file"
+        Write-Host "- $realpath" -ForegroundColor Red
+        Remove-Item -Path $realpath
+    }
 }
 
 <# reg: #>
@@ -69,22 +66,23 @@ $regDelete = @{
     "HKEY_CURRENT_USER\Software\JavaSoft\Prefs" = @("/Jet/Brains./User/Id/On/Machine")
     "HKEY_CURRENT_USER\Software\JavaSoft\Prefs\jetbrains" = @("user_id_on_machine")
 }
-$promptQueue = @();
+$promptQueue = New-Object System.Collections.Generic.List[System.Object];
 
 foreach($path in $regDelete.Keys){
     if ($null -eq $regDelete[$path] -or ($regDelete[$path].Count -eq 0)) {
-        $promptQueue += @("DELETE", "`"$path`"", "/f")
+        $promptQueue.Add("EG RDELETE `"$path`" /f")
     }
     else {
-        foreach ($entries in $regDelete[$path]) {
-            $promptQueue +=  @("DELETE", "`"$path`"", "/v", "`"$entry`"", "/f")
+        foreach ($entry in $regDelete[$path]) {
+            $promptQueue.Add("REG DELETE `"$path`" /v `"$entry`" /f")
         }
     }
 }
 
-
-foreach ($regArgs in $promptQueue) {
-    Write-Host "> $($regArgs -join " ") `n< " -NoNewline -ForegroundColor Green
-    Start-Process reg.exe -ArgumentList $regArgs
+foreach ($prompt in $promptQueue) {
+    Write-Host "> $prompt" -ForegroundColor Green
+    Write-Host "< " -NoNewline -ForegroundColor Green
+    cmd.exe /c $prompt 
 }
+
 Write-Host "Готово! Trial сброшен.\n";
